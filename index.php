283e1aa4936f777ba5a75b5add28c2f5ace07ebf
Eduardo López Orozco <eduardo.lopez.7@gmail.com>
2021-08-13 22:55:09 -0500

    Se modifico el phpinfo

--- index.php
+++ index.php
@@ -4,8 +4,4 @@
 // Muestra toda la información, por defecto INFO_ALL
 phpinfo();
 
-// Muestra solamente la información de los módulos.
-// phpinfo(8) hace exactamente lo mismo.
-phpinfo(INFO_MODULES);
-
 ?>
